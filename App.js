95765af36d7d850bc1d8b012d9edad291d59bfa5
Ankit Raj <developer.ankitraj@gmail.com>
2021-04-11 17:12:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View} from 'react-native';
+
+import Home from './Home/Home';
+
+const App: () => React$Node = () => {
+  return <Home />;
+};
+
+export default App;
